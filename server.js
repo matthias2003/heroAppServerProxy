641bb1ca79ac8346e1ded28b34c1b5d3d0c7ed1d
matthias03 <maciej.kloda98@gmail.com>
2023-10-24 15:52:41 +0200

    Added .env file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3001;
 
@@ -9,11 +10,12 @@ const featuredHeroesIds = [70,265,298,491,542,546];
 let heroes = [];
 
 
+
 const fetchSuperHero = async () => {
     heroes = [];
     for (const heroId of featuredHeroesIds) {
-        const { data:powerstats } = await axios.get(`https://superheroapi.com/api/${ACCESS_TOKEN}/${heroId}/powerstats`);
-        const { data:image } = await axios.get(`https://superheroapi.com/api/${ACCESS_TOKEN}/${heroId}/image`)
+        const { data:powerstats } = await axios.get(`https://superheroapi.com/api/${process.env.ACCESS_TOKEN}/${heroId}/powerstats`);
+        const { data:image } = await axios.get(`https://superheroapi.com/api/${process.env.ACCESS_TOKEN}/${heroId}/image`)
         let data = { name:powerstats.name, powerstats, imgUrl:image.url, id:powerstats.id };
         heroes.push(data)
     }
@@ -22,7 +24,7 @@ const fetchSuperHero = async () => {
 fetchSuperHero();
 
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'http://127.0.0.1:3000'
 }));
 
 app.get("/", async ( req, res ) => {
